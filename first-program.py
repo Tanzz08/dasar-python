767fd072927b95c797f0123635865e660b4c9fb5
tanzz <bustanuldarking23@gmail.com>
2025-02-21 21:30:51 +0800

    first commit

---
+++ first-program.py
@@ -0,0 +1,11 @@
+print ("Hello World")
+
+print(dict([['name','Dicoding'],['age',17]]))
+
+name = 'Dicoding Indonesia'
+print(f"Hello, saya belajar coding di {name}")
+
+
+x = True 
+y = False
+print(x or y)
